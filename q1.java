507bba5851aa11da60e207aa06248475c684ee27
harshophobia <harshbarnawa.info@gmail.com>
2025-10-29 20:52:47 +0530

    Lab 2 ques1 solved

---
+++ q1.java
@@ -0,0 +1,19 @@
+public class q1 {
+    public static void main(String[] args) {
+        int a = 123456;
+        float PI = 3.14f;
+        double longPi = 3.141592;
+        char ch = '*';
+        boolean adult = true;
+        long subscribers = 123456789;
+        short year = 2024;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + PI);
+        System.out.println("Double: " + longPi);
+        System.out.println("Char: " + ch);
+        System.out.println("Boolean: " + adult);
+        System.out.println("Long: " + subscribers);
+        System.out.println("Short: " + year);
+    }
+}
