f5fff6cb7b6b90d356b693a6e0988ae197ebb61d
harshophobia <harshbarnawa.info@gmail.com>
2025-10-29 22:36:38 +0530

    tried one more ques

---
+++ q2.java
@@ -0,0 +1,11 @@
+public class q2 {
+    public static void main(String[] args) {
+        int a = 5,b = 12;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
