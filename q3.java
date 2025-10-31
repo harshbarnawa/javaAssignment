d069013dbed0ca24d47b60b11df2aea505b12a0e
harshophobia <harshbarnawa.info@gmail.com>
2025-10-31 21:38:04 +0530

    ques 3 completed

---
+++ q3.java
@@ -0,0 +1,12 @@
+public class q3 {
+    public static void main(String[] args) {
+        int a = 5, b = 12, c;
+
+        c = b;
+        b = a;
+        a = c;
+
+        System.out.println("a =" + a + ",b ="+ b);
+
+    }
+}
